40065610bb0bf70dbd589a79e21169413f4a8fde
Alexey Leontyev <leontyev@yahoo.com>
2016-09-06 15:48:01 +0300

    add youtube api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyAdu4lwr87ZmW6hhT5-Igcm373xYUjYoKI';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			videos: []
+		}
+
+		YTSearch({key:API_KEY, term: "jimi hendrix"}, videos => {
+			this.setState({videos});
+		})
 	}
 
 	render() {
